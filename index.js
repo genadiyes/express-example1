058eaa65bf75cc3398ce7a32f9846f3825985064
Yevhenii Kazantsev <fedotovtt2t@gmail.com>
2020-05-06 18:32:31 +0300

    Express-example

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const http = require('http');
+const port = 3000;
+const hostname = 'localhost';
+const app = express();
+
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an express Server</h1></body></html>')
+})
+
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+    console.log(`Server running at https://${hostname}:${port}`)
+})
